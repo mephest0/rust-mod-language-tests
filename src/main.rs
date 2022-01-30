b7b374fe833c18c9beefccff4a3ce191d823f632
vevepeter <peter@vev.design>
2022-01-30 22:46:45 +0100

    Added Python example code

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,12 @@ extern crate hlua;
 use hlua::Lua;
 use crate::lua::run_lua;
 
+use pyo3::prelude::*;
+use pyo3::types::IntoPyDict;
+
 mod lua;
 
 fn main() {
-    println!("Hello, world!");
-
     let mut lua = Lua::new();
 
     lua::lua_init(&mut lua);
@@ -16,4 +17,27 @@ fn main() {
 
         println!("(lua says) x: {}", x);
     }
+
+    python(10);
+}
+
+fn python(i: i32) {
+    let _x: PyResult<()> = Python::with_gil(|py| {
+        let fun = PyModule::from_code(
+            py,
+            r#"def fact(n):
+    print(str(n))
+    return 1 if (n==1 or n==0) else n * fact(n - 1);"#,
+            "factorial.py",
+            "factorial",
+        )?.getattr("fact")?;
+
+        // start time
+        for _ in 0..i {
+            let _x: i32 = fun.call1((10,))?.extract()?;
+        }
+        // stop time
+
+        Ok(())
+    });
 }
