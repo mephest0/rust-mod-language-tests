aa1b1ac91e18a70b61cd5ffb9c673ea3992f5391
vevepeter <peter@vev.design>
2022-02-01 10:00:48 +0100

    Renamed variable to cause less confusion with future tests

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,8 @@ mod timer;
 mod loader;
 
 fn main() {
-    let number_of_runs = 5_000_000;
+    let number_of_runs_factorial = 5_000_000;
 
-    lua::lua_factorial_iterative(number_of_runs);
-    python::python_factorial_iterative(number_of_runs);
+    lua::lua_factorial_iterative(number_of_runs_factorial);
+    python::python_factorial_iterative(number_of_runs_factorial);
 }
